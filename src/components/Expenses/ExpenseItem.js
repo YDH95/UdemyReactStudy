bad9e774fe3449ca5b7f0cfe0cbe4f049b07f13b
윤동환 <yundonghwan@yundonghwans-MacBook-Air.local>
2022-09-06 23:31:29 +0900

    컴포넌트 분리

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
+import React, {useState} from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const [title, setTtile] = useState(props.title);
+  const clickHandler = () => {
+    setTtile("!test");
+    console.log(title);
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date = {props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}원</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
