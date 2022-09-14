05c3d43d5e72e7f87fb56439096e50c2b56c908d
윤동환 <yundonghwan@yundonghwans-MacBook-Air.local>
2022-09-14 23:30:42 +0900

    test

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -6,6 +6,7 @@ import "./Chart.css";
 function Chart(props) {
     const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
     const totalMaximum = Math.max(...dataPointValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
